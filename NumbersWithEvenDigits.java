ce8ca3a4644399e1d273586a33013849e4c46e7c
vignesh-kg <vigneshkg515@gmail.com>
2025-04-30 21:00:44 +0300

    Numbers with Even digits

---
+++ NumbersWithEvenDigits.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.function.Function;
+import java.util.function.IntPredicate;
+
+public class NumbersWithEvenDigits {
+  public static void main(String[] args) {
+    int[] input = new int[]{12,345,2,6,7896};
+    Solution s = new Solution();
+    System.out.println(s.findNumbers(input));
+  }
+}
+
+class Solution {
+  public int findNumbers(int[] nums) {
+    return (int) Arrays.stream(nums).filter(num -> {
+      int count = 0;
+      for(;num != 0; num /= 10){
+        ++count;
+      }
+      return count % 2 == 0;
+    }).count();
+  }
+}

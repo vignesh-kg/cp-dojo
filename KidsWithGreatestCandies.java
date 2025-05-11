0a35011ae0284989933a9347bdb3dfaef20481a2
vignesh-kg <vigneshkg515@gmail.com>
2025-05-11 18:42:14 +0300

    refactor

--- KidsWithGreatestCandies.java
+++ KidsWithGreatestCandies.java
@@ -1,9 +1,7 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.OptionalInt;
 import java.util.Scanner;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class KidsWithGreatestCandies {

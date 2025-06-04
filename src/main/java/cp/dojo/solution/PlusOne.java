b2c2fc1fff489a1209f159d0a1d53f3840202d4c
Vignesh Ganapathy <vigneshkg515@gmail.com>
2025-06-04 18:14:47 +0300

    PlusOne.java

---
+++ src/main/java/cp/dojo/solution/PlusOne.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class PlusOne {
+  public static void main(String[] args){
+    PlusOne s = new PlusOne();
+    System.out.println(Arrays.toString(s.plusOne(new int[]{8,9,9})));
+    System.out.println(Arrays.toString(s.plusOne(new int[]{9,9,9})));
+  }
+
+  public int[] plusOne(int[] digits) {
+    for(int i = digits.length-1; i >= 0; --i){
+      if(digits[i] + 1 != 10){
+        ++digits[i];
+        return digits;
+      }
+      digits[i] = 0;
+    }
+    int[] newDigits = new int[digits.length+1];
+    newDigits[0] = 1;
+    return newDigits;
+  }
+}

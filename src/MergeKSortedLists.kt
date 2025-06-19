4888ee2d8285c504061e9221d0c02f0a5ba7a6cb
vignesh-kg <vigneshkg515@gmail.com>
2025-06-19 19:32:55 +0300

    refactor

--- src/MergeKSortedLists.kt
+++ src/MergeKSortedLists.kt
@@ -1,11 +1,11 @@
 import structs.ListNode
 
-fun main(){
-    val l1 : ListNode = ListNode(1, ListNode(4, ListNode(5)))
-    val l2 : ListNode = ListNode(1, ListNode(3, ListNode(4)))
-    val l3 : ListNode = ListNode(2, ListNode(6))
-    var head : ListNode? = mergeKLists(arrayOf(l1, l2, l3))
-    while(head != null){
+fun main() {
+    val l1: ListNode = ListNode(1, ListNode(4, ListNode(5)))
+    val l2: ListNode = ListNode(1, ListNode(3, ListNode(4)))
+    val l3: ListNode = ListNode(2, ListNode(6))
+    var head: ListNode? = mergeKLists(arrayOf(l1, l2, l3))
+    while (head != null) {
         print("${head.`val`} ")
         head = head.next
     }
@@ -13,35 +13,35 @@ fun main(){
 }
 
 fun mergeKLists(lists: Array<ListNode?>): ListNode? {
-    var dummy : ListNode? = ListNode(-1)
-    val copy : ListNode? = dummy
-    for(node in lists){
+    var dummy: ListNode? = ListNode(-1)
+    val copy: ListNode? = dummy
+    for (node in lists) {
         dummy?.next = node
-        while(dummy?.next != null){
+        while (dummy?.next != null) {
             dummy = dummy.next
         }
     }
     return sort(copy?.next)
 }
 
-fun sort(node: ListNode?) : ListNode?{
-    if(node == null || node.next == null){
+fun sort(node: ListNode?): ListNode? {
+    if (node == null || node.next == null) {
         return node
     }
 
-    val midNode : ListNode? = findMidNode(node)
-    val right : ListNode? = midNode?.next
+    val midNode: ListNode? = findMidNode(node)
+    val right: ListNode? = midNode?.next
     midNode?.next = null
     return merge(sort(node), sort(right))
 }
 
-fun merge(l1 : ListNode?, l2 : ListNode?): ListNode?{
-    var dummy : ListNode? = ListNode(-1)
-    val copy : ListNode? = dummy
-    var h1 : ListNode? = l1
-    var h2 : ListNode? = l2
-    while (h1 != null && h2 != null){
-        if(h1.`val` < h2.`val`){
+fun merge(l1: ListNode?, l2: ListNode?): ListNode? {
+    var dummy: ListNode? = ListNode(-1)
+    val copy: ListNode? = dummy
+    var h1: ListNode? = l1
+    var h2: ListNode? = l2
+    while (h1 != null && h2 != null) {
+        if (h1.`val` < h2.`val`) {
             dummy?.next = h1
             h1 = h1.next
         } else {
@@ -50,7 +50,7 @@ fun merge(l1 : ListNode?, l2 : ListNode?): ListNode?{
         }
         dummy = dummy?.next
     }
-    if(h1 != null){
+    if (h1 != null) {
         dummy?.next = h1
     } else {
         dummy?.next = h2
@@ -58,14 +58,14 @@ fun merge(l1 : ListNode?, l2 : ListNode?): ListNode?{
     return copy?.next
 }
 
-fun findMidNode(node: ListNode?) : ListNode?{
-    if(node == null || node.next == null){
+fun findMidNode(node: ListNode?): ListNode? {
+    if (node == null || node.next == null) {
         return node
     }
 
-    var slow : ListNode? = node
-    var fast : ListNode? = node.next
-    while(fast != null && fast.next != null){
+    var slow: ListNode? = node
+    var fast: ListNode? = node.next
+    while (fast != null && fast.next != null) {
         slow = slow?.next
         fast = fast.next?.next
     }

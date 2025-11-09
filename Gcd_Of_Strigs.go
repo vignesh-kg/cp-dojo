b710be4d48362efc1b9611d240b480fc946e625d
vignesh-kg <vigneshkg515@gmail.com>
2025-11-09 11:30:55 +0530

    Gcd_of_strings.go

---
+++ Gcd_Of_Strigs.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Println(gcdOfStrings("ABCABC", "ABC"))
+}
+
+func gcdOfStrings(str1 string, str2 string) string {
+	if str1+str2 != str2+str1{
+		return ""
+	}
+
+	var gcd func(int, int) int;
+	gcd = func(a int, b int) int{
+		if(b != 0){
+			return gcd(b, a%b)
+		}
+		return a
+	}
+
+	return str1[:gcd(len(str1), len(str2))]
+}

2e9d0ec9e2a908dfd8d704196e1d95b3bce64ac6
vignesh-kg <vigneshkg515@gmail.com>
2026-01-25 12:05:47 +0530

    Intersection Of Two LL

---
+++ IntersectionOfTwoLL.java
@@ -0,0 +1,19 @@
+import structs.ListNode;
+
+public class IntersectionOfTwoLL {
+    public static void main(String[] args){
+        ListNode intersect = new ListNode(8, new ListNode(4, new ListNode(5)));
+        ListNode headA = new ListNode(4, new ListNode(1, intersect));
+        ListNode headB = new ListNode(5, new ListNode(6, new ListNode(1, intersect)));
+        System.out.println(new IntersectionOfTwoLL().getIntersectionNode(headA, headB).val);
+    }
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode temp1 = headA, temp2 = headB;
+        while(temp1 != temp2){
+            temp1 = temp1 == null ? headB : temp1.next;
+            temp2 = temp2 == null ? headA : temp2.next;
+        }
+        return temp1;
+    }
+}
